8017b14f6f71902043562468e38eb6e028c56426
Hamza-rgb-hub <hb1967914@gmail.com>
2026-04-28 15:33:28 +0500

    update API

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,3 +1,21 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "https://fragy.onrender.com/api",
+//   withCredentials: true,
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return req;
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
@@ -6,10 +24,12 @@ const API = axios.create({
 });
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (user?.token) {
+    req.headers.Authorization = `Bearer ${user.token}`;
   }
+
   return req;
 });
 
